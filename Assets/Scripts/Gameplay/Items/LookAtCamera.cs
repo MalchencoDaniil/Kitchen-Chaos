2b61f95f5dfa65c456071c6aaef742b6ebd20211
Malchenko Daniil <109500163+MalchencoDaniil@users.noreply.github.com>
2025-09-26 20:18:49 +0300

    Look at camera feature

---
+++ Assets/Scripts/Gameplay/Items/LookAtCamera.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace KitchenChaos.Items
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private enum Mode
+        {
+            LookAt,
+            LookAtInverted,
+            CameraForward,
+            CameraForwardInverted
+        }
+
+        [SerializeField] private Mode _lookAtMode = Mode.CameraForward;
+
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+            _mainCamera = FindAnyObjectByType<Camera>();
+        }
+
+        private void LateUpdate()
+        {
+            switch (_lookAtMode)
+            {
+                case Mode.LookAt:
+                    transform.LookAt(_mainCamera.transform.position);
+                    break;
+                case Mode.LookAtInverted:
+                    Vector3 _directionFromCamera = transform.position - _mainCamera.transform.position;
+                    transform.LookAt(transform.position + _directionFromCamera);
+                    break;
+                case Mode.CameraForward:
+                    transform.forward = _mainCamera.transform.forward;
+                    break;
+                case Mode.CameraForwardInverted:
+                    transform.forward = -_mainCamera.transform.forward;
+                    break;
+            }
+        }
+    }
+}
