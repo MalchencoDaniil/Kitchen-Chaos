53572a2315a25357b9389ba5f8286959832cd9b0
Malchenko Daniil <109500163+MalchencoDaniil@users.noreply.github.com>
2025-09-28 16:47:43 +0300

    Spawn plates logic and visual

---
+++ Assets/Scripts/Gameplay/Items/Counters/Visual/PlatesCounterVisual.cs
@@ -0,0 +1,49 @@
+using KitchenChaos.Items.Counters.Plates;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenChaos.Items.Counters.Visual
+{
+    public class PlatesCounterVisual : CounterVisual
+    {
+        [SerializeField] private PlatesCounter _platesCounter;
+        [SerializeField] private Transform _counterTopPoint;
+
+        [Space(10)]
+        [SerializeField] private Transform _platesVisualPrefab;
+
+        private List<GameObject> _plateVisualGameObjectList;
+
+        private void Awake()
+        {
+            _plateVisualGameObjectList = new List<GameObject>();
+
+            _platesCounter.OnPlateSpawned += VisualView;
+            _platesCounter.OnPlateRemove += OnPlateRemove;
+        }
+
+        public override void VisualView(object sender, EventArgs eventArgs)
+        {
+            Transform _prefabToSpawn = Instantiate(_platesVisualPrefab, _counterTopPoint);
+
+            float _plateOffsetY = 0.1f;
+            _prefabToSpawn.localPosition = new Vector3(0, _plateOffsetY * _plateVisualGameObjectList.Count, 0);
+            _plateVisualGameObjectList.Add(_prefabToSpawn.gameObject);
+        }
+
+        public void OnPlateRemove(object sender, EventArgs eventArgs)
+        {
+            GameObject _removeObject = _plateVisualGameObjectList[_plateVisualGameObjectList.Count - 1];
+            _plateVisualGameObjectList.Remove(_removeObject);
+            Destroy(_removeObject);
+        }
+
+        private void OnDestroy()
+        {
+            _platesCounter.OnPlateSpawned -= VisualView;
+            _platesCounter.OnPlateRemove -= OnPlateRemove;
+        }
+    }
+}
